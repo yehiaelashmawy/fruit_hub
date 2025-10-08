6e016a0ebda2e7cc8c329874cf3bae8e14c34cf0
yehiaelashmawy <elashmawyyehia@gmail.com>
2025-10-08 17:18:32 +0300

    on generate route

--- lib/features/splash/presentation/views/splash_view.dart
+++ lib/features/splash/presentation/views/splash_view.dart
@@ -3,7 +3,7 @@ import 'package:fruit_hub/features/splash/presentation/views/widgets/splash_view
 
 class SplashView extends StatelessWidget {
   const SplashView({super.key});
-
+  static const routeName = 'splash';
   @override
   Widget build(BuildContext context) {
     return const Scaffold(body: SplashViewBody());
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:fruit_hub/features/splash/presentation/views/splash_view.dart';
 
+import 'core/helper_funcations/on_generate_routs.dart';
+
 void main() {
   runApp(const FruitHub());
 }
@@ -10,6 +12,9 @@ class FruitHub extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: SplashView());
+    return const MaterialApp(
+      onGenerateRoute: onGenerateRoute,
+      initialRoute: SplashView.routeName,
+    );
   }
 }
