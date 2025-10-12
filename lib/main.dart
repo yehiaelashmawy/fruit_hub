db0904a16a356c4d7f191d7a8c1233d54dc97eae
yehiaelashmawy <elashmawyyehia@gmail.com>
2025-10-13 00:06:07 +0300

    add font family

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class FruitHub extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(fontFamily: 'cairo'),
       localizationsDelegates: [
         S.delegate,
         GlobalMaterialLocalizations.delegate,
