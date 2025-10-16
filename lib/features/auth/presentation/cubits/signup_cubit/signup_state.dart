06da03330db60d8134a25e0628c08aa658ec2be3
yehiaelashmawy <elashmawyyehia@gmail.com>
2025-10-16 22:36:00 +0300

    create signup cubit

---
+++ lib/features/auth/presentation/cubits/signup_cubit/signup_state.dart
@@ -0,0 +1,20 @@
+part of 'signup_cubit.dart';
+
+@immutable
+sealed class SignupState {}
+
+final class SignupInitial extends SignupState {}
+
+final class SignupLoading extends SignupState {}
+
+final class SignupSuccess extends SignupState {
+  final UserEntity userEntity;
+
+  SignupSuccess({required this.userEntity});
+}
+
+final class SignupFailure extends SignupState {
+  final String message;
+
+  SignupFailure({required this.message});
+}
