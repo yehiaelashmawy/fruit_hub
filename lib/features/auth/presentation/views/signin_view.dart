5a9cdd23d589ed20f300578905af01bd811c0241
yehiaelashmawy <elashmawyyehia@gmail.com>
2025-10-17 21:10:34 +0300

    email and password login

--- lib/features/auth/presentation/views/signin_view.dart
+++ lib/features/auth/presentation/views/signin_view.dart
@@ -4,7 +4,7 @@ import 'package:fruit_hub/core/service/get_it_service.dart';
 import 'package:fruit_hub/core/widgets/custom_app_bar.dart';
 import 'package:fruit_hub/features/auth/domain/repos/auth_repo.dart';
 import 'package:fruit_hub/features/auth/presentation/cubits/signin_cubit/signin_cubit.dart';
-import 'package:fruit_hub/features/auth/presentation/views/widgets/signin_view_body.dart';
+import 'package:fruit_hub/features/auth/presentation/views/widgets/signin_view_body_bloc_consumer.dart';
 
 class SigninView extends StatelessWidget {
   const SigninView({super.key});
@@ -17,7 +17,7 @@ class SigninView extends StatelessWidget {
       create: (context) => SigninCubit(getIt.get<AuthRepo>()),
       child: Scaffold(
         appBar: buildAppBar(context, title: 'تسجيل الدخول'),
-        body: const SigninViewBody(),
+        body: SigninViewBodyBlocConsumer(),
       ),
     );
   }
--- lib/features/auth/presentation/views/widgets/signin_view_body.dart
+++ lib/features/auth/presentation/views/widgets/signin_view_body.dart
@@ -1,78 +1,104 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fruit_hub/core/utils/app_colors.dart';
 import 'package:fruit_hub/core/utils/app_images.dart';
 import 'package:fruit_hub/core/utils/app_text_style.dart';
 import 'package:fruit_hub/core/widgets/custom_button.dart';
 import 'package:fruit_hub/core/widgets/custom_text_form_feild.dart';
+import 'package:fruit_hub/core/widgets/password_feild.dart';
+import 'package:fruit_hub/features/auth/presentation/cubits/signin_cubit/signin_cubit.dart';
 import 'package:fruit_hub/features/auth/presentation/views/widgets/donnot_have_account_widget.dart';
 import 'package:fruit_hub/features/auth/presentation/views/widgets/or_divider.dart';
 import 'package:fruit_hub/features/auth/presentation/views/widgets/social_login_boutton.dart';
 
-class SigninViewBody extends StatelessWidget {
+class SigninViewBody extends StatefulWidget {
   const SigninViewBody({super.key});
 
+  @override
+  State<SigninViewBody> createState() => _SigninViewBodyState();
+}
+
+class _SigninViewBodyState extends State<SigninViewBody> {
+  late String email, password;
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16.0),
       child: SingleChildScrollView(
-        child: Column(
-          spacing: 4,
-          children: [
-            SizedBox(height: 24),
-            CustomTextFormFeild(
-              hintText: 'البريد الإلكتروني',
-              textInputType: TextInputType.emailAddress,
-            ),
-            SizedBox(height: 16),
-            CustomTextFormFeild(
-              hintText: 'كلمة المرور',
-              textInputType: TextInputType.visiblePassword,
-              suffixIcon: IconButton(
-                icon: Icon(Icons.remove_red_eye, color: Color(0xFFC9CECF)),
-                onPressed: () {},
+        child: Form(
+          key: formKey,
+          autovalidateMode: autovalidateMode,
+          child: Column(
+            spacing: 4,
+            children: [
+              SizedBox(height: 24),
+              CustomTextFormFeild(
+                onSaved: (value) {
+                  email = value!;
+                },
+                hintText: 'البريد الإلكتروني',
+                textInputType: TextInputType.emailAddress,
               ),
-            ),
-            SizedBox(height: 16),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Text(
-                  'نسيت كلمة المرور؟',
-                  style: TextStyles.semiBold13.copyWith(
-                    height: 1.7,
-                    color: AppColors.lightPrimaryColor,
+              SizedBox(height: 16),
+              PasswordFeild(
+                onSaved: (value) {
+                  password = value!;
+                },
+              ),
+              SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    'نسيت كلمة المرور؟',
+                    style: TextStyles.semiBold13.copyWith(
+                      height: 1.7,
+                      color: AppColors.lightPrimaryColor,
+                    ),
                   ),
-                ),
-              ],
-            ),
-            SizedBox(height: 33),
-            CustomButton(onPressed: () {}, text: 'تسجيل دخول '),
-            SizedBox(height: 33),
-            DonnotHaveAccountWidget(),
-            SizedBox(height: 37),
-            OrDivider(),
-            SizedBox(height: 21),
+                ],
+              ),
+              SizedBox(height: 33),
+              CustomButton(
+                onPressed: () {
+                  if (formKey.currentState!.validate()) {
+                    formKey.currentState!.save();
+                    context.read<SigninCubit>().signin(email, password);
+                  } else {
+                    autovalidateMode = AutovalidateMode.always;
+                    setState(() {});
+                  }
+                },
+                text: 'تسجيل دخول ',
+              ),
+              SizedBox(height: 33),
+              DonnotHaveAccountWidget(),
+              SizedBox(height: 37),
+              OrDivider(),
+              SizedBox(height: 21),
 
-            SocialLoginBoutton(
-              onPressed: () {},
-              image: Assets.imagesGoogleIcon,
-              title: 'تسجيل بواسطة جوجل',
-            ),
-            SizedBox(height: 16),
-            SocialLoginBoutton(
-              onPressed: () {},
-              image: Assets.imagesAppleIcon,
-              title: 'تسجيل بواسطة ابل',
-            ),
-            SizedBox(height: 16),
-            SocialLoginBoutton(
-              onPressed: () {},
-              image: Assets.imagesFacebookIcon,
-              title: 'تسجيل بواسطة فيسبوك',
-            ),
-            SizedBox(height: 66),
-          ],
+              SocialLoginBoutton(
+                onPressed: () {},
+                image: Assets.imagesGoogleIcon,
+                title: 'تسجيل بواسطة جوجل',
+              ),
+              SizedBox(height: 16),
+              SocialLoginBoutton(
+                onPressed: () {},
+                image: Assets.imagesAppleIcon,
+                title: 'تسجيل بواسطة ابل',
+              ),
+              SizedBox(height: 16),
+              SocialLoginBoutton(
+                onPressed: () {},
+                image: Assets.imagesFacebookIcon,
+                title: 'تسجيل بواسطة فيسبوك',
+              ),
+              SizedBox(height: 66),
+            ],
+          ),
         ),
       ),
     );
