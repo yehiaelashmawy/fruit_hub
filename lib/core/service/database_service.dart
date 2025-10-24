3ec46e8c638e27dfbf19b97c44d5ca2668fe6b51
yehiaelashmawy <elashmawyyehia@gmail.com>
2025-10-24 20:47:18 +0300

    create database service

---
+++ lib/core/service/database_service.dart
@@ -0,0 +1,6 @@
+abstract class DatabaseService {
+  Future<void> addData({
+    required String path,
+    required Map<String, dynamic> data,
+  });
+}
--- lib/core/service/firestore_service.dart
+++ lib/core/service/firestore_service.dart
@@ -1,1 +1,14 @@
-class FirestoreService {}
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fruit_hub/core/service/database_service.dart';
+
+class FirestoreService implements DatabaseService {
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  @override
+  Future<void> addData({
+    required String path,
+    required Map<String, dynamic> data,
+  }) async {
+    await firestore.collection(path).add(data);
+  }
+}
