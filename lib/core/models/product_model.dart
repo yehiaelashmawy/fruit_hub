27cd7cab5037a3bce53b1598f6f34f7abfb43414
yehiaelashmawy <elashmawyyehia@gmail.com>
2026-01-12 21:53:55 +0200

    add selling count to product model

--- lib/core/models/product_model.dart
+++ lib/core/models/product_model.dart
@@ -1,6 +1,3 @@
-import 'dart:io';
-
-import 'package:fruit_hub/core/entites/product_entity.dart';
 import 'package:fruit_hub/core/models/review_model.dart';
 
 class ProductModel {
@@ -9,7 +6,7 @@ class ProductModel {
   final int productPrice;
   final String productDescription;
   final bool isFeatured;
-  final File productImage;
+  // final File productImage;
   String? imageUrl;
   final int expiredMonth;
   final bool isOrganic;
@@ -17,6 +14,7 @@ class ProductModel {
   final num avgRating = 0;
   final num ratingCount = 0;
   final int unitAmount;
+  final num sellingCount;
   final List<ReviewModel> reviews;
 
   ProductModel({
@@ -24,33 +22,31 @@ class ProductModel {
     required this.productCode,
     required this.productPrice,
     required this.productDescription,
-    required this.productImage,
+    // required this.productImage,
     required this.reviews,
     required this.isFeatured,
-    this.isOrganic = false,
+    required this.isOrganic,
+    required this.sellingCount,
 
     required this.expiredMonth,
     required this.numberOfCalories,
     required this.unitAmount,
     this.imageUrl,
   });
-
-  factory ProductModel.fromEntity(ProductEntity addProductInputEntity) {
+  factory ProductModel.fromJson(Map<String, dynamic> json) {
     return ProductModel(
-      productName: addProductInputEntity.productName,
-      productCode: addProductInputEntity.productCode,
-      productPrice: addProductInputEntity.productPrice,
-      productDescription: addProductInputEntity.productDescription,
-      productImage: addProductInputEntity.productImage,
-      isFeatured: addProductInputEntity.isFeatured,
-      isOrganic: addProductInputEntity.isOrganic,
-      reviews: addProductInputEntity.reviews
-          .map((e) => ReviewModel.fromEntity(e))
-          .toList(),
-      imageUrl: addProductInputEntity.imageUrl,
-      expiredMonth: addProductInputEntity.expiredMonth,
-      numberOfCalories: addProductInputEntity.numberOfCalories,
-      unitAmount: addProductInputEntity.unitAmount,
+      productName: json['productName'],
+      productCode: json['productCode'],
+      productPrice: json['productPrice'],
+      productDescription: json['productDescription'],
+      imageUrl: json['imageUrl'],
+      isFeatured: json['isFeatured'],
+      expiredMonth: json['expiredMonth'],
+      numberOfCalories: json['numberOfCalories'],
+      unitAmount: json['unitAmount'],
+      isOrganic: json['isOrganic'],
+      sellingCount: json['sellingCount'],
+      reviews: json['reviews'].map((e) => ReviewModel.fromJson(e)).toList(),
     );
   }
 
@@ -66,6 +62,7 @@ class ProductModel {
       'numberOfCalories': numberOfCalories,
       'unitAmount': unitAmount,
       'isOrganic': isOrganic,
+      'sellingCount': sellingCount,
       'reviews': reviews.map((e) => e.toJson()).toList(),
     };
   }
