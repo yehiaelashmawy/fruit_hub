e974780f44ecc63ba137e234ff497266757e33c5
yehiaelashmawy <elashmawyyehia@gmail.com>
2026-01-15 00:02:47 +0200

    best selling fruit view done

--- lib/features/best_selling_fruit/presentation/view/best_selling_fruit_view.dart
+++ lib/features/best_selling_fruit/presentation/view/best_selling_fruit_view.dart
@@ -1,16 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:fruit_hub/features/best_selling_fruit/presentation/view/widgets/best_selling_fruit_view_body.dart';
-
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruit_hub/core/cubits/products_cubit/products_cubit.dart';
+import '../../../../core/repos/products_repo/products_repo.dart';
+import '../../../../core/service/get_it_service.dart';
 import '../../../../core/widgets/build_app_bar.dart';
+import '../../../home/presentation/views/widget/product_grid_view_bloc_builder.dart';
 
 class BestSellingFruitView extends StatelessWidget {
   const BestSellingFruitView({super.key});
   static const routeName = 'best-selling';
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: buildAppBar(context, title: 'الاكثر مبيعا'),
-      body: BestSellingFruitViewBody(),
+    return BlocProvider(
+      create: (context) =>
+          ProductsCubit(getIt.get<ProductsRepo>())..getBestSellingProducts(),
+      child: Scaffold(
+        appBar: buildAppBar(context, title: 'الاكثر مبيعا'),
+        body: CustomScrollView(slivers: [ProductGridViewBlocBuilder()]),
+      ),
     );
   }
 }
